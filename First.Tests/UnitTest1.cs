b979b109e73827c57c6453a53e1bf12b377cac8f
Teri_anric <2005ahi2005@gmai.com>
2024-01-16 17:54:10 +0200

    init

---
+++ First.Tests/UnitTest1.cs
@@ -0,0 +1,41 @@
+global using NUnit.Framework;
+namespace First.Test;
+
+
+public class Tests
+{
+    
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+
+    [Test]
+    [TestCase(9, 9)]
+    [TestCase(4, 4)]
+    [TestCase(8, 8)]
+    [TestCase(1, 1)]
+    [TestCase(5, 5)]
+    [TestCase(8, 8)]
+    [TestCase(64, 64)]
+    [TestCase(10, 10)]
+    [TestCase(27, 72)]
+    [TestCase(27, 72)]
+    [TestCase(17, 71)]
+    [TestCase(97, 97)]
+    [TestCase(193, 931)]
+    [TestCase(483, 843)]
+    [TestCase(762, 762)]
+    [TestCase(7416, 7641)]
+    [TestCase(8530, 8530)]
+    [TestCase(39035, 95330)]
+    [TestCase(75626, 76652)]
+    [TestCase(90783, 98730)]
+    public static void FirstProgram_MaxIntegerByNumDigts_ReturnBool(int in_num, int out_result)
+    {
+        int result = FirstProgram.MaxIntegerByNumDigts(in_num);
+        Assert.That(out_result, Is.EqualTo(result));
+    }
+}
---
+++ First/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace First
+{
+    public static class FirstProgram
+    {
+
+        public static int MaxIntegerByNumDigts(int num)
+        {
+            char[] arr = num.ToString().ToCharArray();
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            return int.Parse(arr);
+        }
+
+        public static int Main(string[] argv)
+        {
+            string? input_line = Console.ReadLine();
+
+            if (input_line is null)
+            {
+                return -1;
+            }
+        
+            int input_num = int.Parse(input_line);
+
+            int result = MaxIntegerByNumDigts(input_num);
+
+            Console.WriteLine(result);
+
+            return 0;
+        }
+    }
+}
